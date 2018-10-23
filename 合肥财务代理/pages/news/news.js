f3df9d8988578003c6f02be78530f355c3c68dc8
zlqGitHub <1509874021@qq.com>
2018-10-23 17:48:24 +0800

    合肥财务代理

---
+++ 合肥财务代理/pages/news/news.js
@@ -0,0 +1,19 @@
+// pages/news/news.js
+import service from "../../util/service";
+import config from "../../util/config";
+Page({
+
+  data: {
+    servicesObjects: []
+  },
+
+  onLoad: function (options) {
+    service.getObjectsInfo(config.newsTable, (res) => {
+      console.log(res.data);
+      this.setData({
+        servicesObjects: res.data.objects
+      });
+    }, 10000)
+  },
+
+})
