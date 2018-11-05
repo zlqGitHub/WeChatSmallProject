f7644ee5cc6c099fd61d51eb92d0f49659c06ad0
zlqGitHub <1509874021@qq.com>
2018-11-05 17:54:34 +0800

    附近家政简单页面小程序

---
+++ 附近家政/pages/serve/serve.js
@@ -0,0 +1,103 @@
+// pages/serve/serve.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grids: [
+      {
+        img:'../../resources/images/baojie.png',
+        title:'保洁清洗'
+      },
+      {
+        img: '../../resources/images/ren.png',
+        title: '保姆月嫂'
+      },
+      {
+        img: '../../resources/images/guandao.png',
+        title: '管道疏通'
+      },
+      {
+        img: '../../resources/images/tv.png',
+        title: '家电装修'
+      },
+      {
+        img: '../../resources/images/shafa.png',
+        title: '家具修装'
+      },
+      {
+        img: '../../resources/images/car.png',
+        title: '搬家运输'
+      }
+    ],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+
+  
+})
